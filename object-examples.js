2118eef74c53c7779fd0e1390cf208059d1bf541
Megan McBride <megan.mcbride@colorado.edu>
2021-03-31 20:40:56 -0600

    Add objects to file

--- object-examples.js
+++ object-examples.js
@@ -8,3 +8,19 @@ var car = {
 
 console.log(car.year);
 console.log(car.name);
+
+var animal = {
+  name : 'Llama',
+  type : 'mammal',
+  caution : 'beware of spit',
+  maxHeight : 76,
+  maxWeight : 456,
+}
+
+
+var customer = {
+  name : 'Sue',
+  familiy : 5,
+  priceLImit : 54000,
+  colorPreference : 'blue',
+}
